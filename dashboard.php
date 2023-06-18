f5600ee2a87259494f2bc41cbd419ee29d0035b8
Bladeyboy54 <221146@virtualwindow.co.za>
2023-06-18 18:25:23 +0200

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta   charset="UTF-8">
+    <meta   http-equiv="X-UA-Compatible" 
+            content="IE=edge">
+    <meta   name="viewport" 
+            content="width=device-width, initial-scale=1.0">
+    <meta   studentName="Bladen Lehnberg"
+            studentNumber="221146"
+            class="DV200_T2"
+            lecturer="Tsungai Katsuro">
+    <link   rel="stylesheet" 
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link   rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@4.0.8/css/line.min.css">
+    <link   rel="stylesheet"
+            href="style.css">
+
     <title>Document</title>
 </head>
 <body>
+  <div class="container">
     <h1>Dashboard</h1>
+  </div>
 </body>
 </html>
