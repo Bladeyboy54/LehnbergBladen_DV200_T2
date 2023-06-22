07ffc41c5b6dbc070c4c766136a518ec774f3297
Bladeyboy54 <221146@virtualwindow.co.za>
2023-06-22 17:06:52 +0200

    doctors (add + delete func) (update non-func)

---
+++ doctor-delete.php
@@ -0,0 +1,11 @@
+<?php
+    include 'db.php';
+
+    $doctor_id = $_GET['doctor_id'];
+
+    $sql = "DELETE FROM doctors WHERE doctor_id = $doctor_id";
+
+    $conn->query($sql);
+    $conn->close();
+    header("location: doctors.php");
+?>
---
+++ doctor-update.php
@@ -0,0 +1,23 @@
+<?php
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    include 'db.php';
+
+    $doctor_id = $_POST['doctor_id'];
+    $profile_img = $_POST['profile_img'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $specialization = $_POST['specialization'];
+
+    $sql = "update doctors set profile_img='$profile_img', name='$name', surname='$surname', age='$age', gender='$gender', email='$email', phone='$phone', specialization='$specialization' where doctor_id=$doctor_id";
+
+    $result = $conn->query($sql);
+    $conn->close();
+    header("location: doctors.php");
+?>
